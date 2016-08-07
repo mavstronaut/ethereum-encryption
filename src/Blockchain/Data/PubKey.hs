6ace3aa95481f77b7fdf1906031eb73c7f960957
Jamshid <qq@qq.com>
2016-08-07 13:00:43 -0700

    Cleanup of the PubKey functions, an beginning to add the real bootnodes.

--- src/Blockchain/Data/PubKey.hs
+++ src/Blockchain/Data/PubKey.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Blockchain.Data.PubKey (
+  stringToPoint,
   pointToBytes,
   bytesToPoint,
   pubKeyToBytes
@@ -7,11 +10,42 @@ module Blockchain.Data.PubKey (
 
 import Crypto.Types.PubKey.ECC
 import Data.Bits
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Base16 as B16
+import qualified Data.ByteString.Char8 as BC
 import Data.Maybe
 import Data.Word
 import qualified Network.Haskoin.Internals as H
 
+import qualified Blockchain.Colors as CL
+import Blockchain.Data.RLP
 import Blockchain.ExtWord
+import Blockchain.Format
+
+stringToPoint::String->Point
+stringToPoint string =
+  case B16.decode $ BC.pack string of
+   (val, "") -> bytesToPoint $ B.unpack val
+   _ -> error $ "stringToPoint called with malformed string: " ++ string
+
+instance Format Point where
+  format x = 
+    CL.yellow (take 30 (format $ B.pack $ pointToBytes x) ++ "...")
+
+instance RLPSerializable Point where
+  rlpEncode (Point x y) =
+    rlpEncode $ B.pack $ (word256ToBytes $ fromInteger x) ++ (word256ToBytes $ fromInteger y)
+  rlpEncode PointO = error "rlpEncode for Point called for PointO"
+  rlpDecode o =
+    Point (toInteger $ bytesToWord256 $ B.unpack x) (toInteger $ bytesToWord256 $ B.unpack y)
+    where
+      (x, y) = B.splitAt 32 $ rlpDecode o
+
+{-
+instance Format Point where
+  format (Point x y) = padZeros 64 (showHex x "") ++ padZeros 64 (showHex y "")
+  format PointO = "PointO"
+-}
 
 pointToBytes::Point->[Word8]
 pointToBytes (Point x y) = intToBytes x ++ intToBytes y
